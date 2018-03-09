1985e30899408c680c442895147de883e18cdcf3
m4g4 <martin.gabor.sk@gmail.com>
2018-03-09 16:15:07 +0100

    Initial commit. Project setup.

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import DevTools from 'mobx-react-devtools';
+
+class App extends React.Component {
+
+    render() {
+        const devTools = process.env.NODE_ENV === 'development' ? (<DevTools />) : undefined;
+
+        return (
+            <div>
+                <Button />
+                {devTools}
+            </div>);
+    }
+}
+
+export default App;
